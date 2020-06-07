4f4a09081d48d2b590260e48b29bb6bfc85b158a
Semen <bethesda.stillxxxx@gmail.com>
2020-06-07 14:11:02 +0300

    Добавил слайдер на about

--- html/js/index.js
+++ html/js/index.js
@@ -2,3 +2,12 @@ function test(msg) {
 	alert(msg)
 }
 
+function setSlide(indexOf) {
+	var slides = document.getElementsByClassName("slide");
+
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	slides[indexOf].style.display = "block";
+}
